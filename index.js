80857ded46a5e6a04644bee752cbce6d9aea095a
Replit user <>
2023-01-09 14:00:51 +1100

    uploaded to personal github

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' }).single("upfile");
 
 var app = express();
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +14,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse", (req,res) => {
+  upload(req,res,(err) => {
+    if (err) return console.log(err);
+    let responseObject = {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    }
+    res.send(responseObject);
+  });
+})
 
 
 const port = process.env.PORT || 3000;
